9f88076632e0d2ce1a6031d5da3685fffd3f575e
ganesh128kb <ganesh128kb@gmail.com>
2015-09-03 23:21:27 +0530

    make it better

--- server.js
+++ server.js
@@ -1,66 +1,67 @@
 var express = require('express');
-var mysql = require('mysql');
 var session = require('express-session');
 var bodyParser = require('body-parser');
+var mysql = require('mysql');
 
 /*------ Initialize ------*/
 
 var app = express();
 var ip = require('ip');
-var crypto = require('crypto'),
-    algorithm = 'aes-256-ctr',
-    password = 'd6F3Efeq';
-	
-/*--------Encrypt--------*/
-function encrypt(text){
-  var cipher = crypto.createCipher(algorithm,password)
-  var crypted = cipher.update(text,'utf8','hex')
-  crypted += cipher.final('hex');
-  return crypted;
-}
 
 /*--------Body Parsing--------*/
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({extended:true}));
 
 /*----------Session--------*/
 var sess;
-app.use(session({secret: 'keyboard cat', resave: false, saveUninitialized: true}));
+app.use(session({secret: 'ssshhhhh',saveUninitialized: true,resave: true}));
 
 /*--------Template Engine--------*/
 app.engine('html',require('ejs').renderFile); // render HTML Files
 app.use('/public',express.static(__dirname+'/public')); // Folder Access
 
-/*--------Mysql Connection--------*/
+/*--------Mysql Connection--------
 var connection = mysql.createPool({
     connectionLimit: 3,
     host: 'us-cdbr-iron-east-02.cleardb.net',
     user: 'bd2ae73e3c90c6',
     password: '37758202',
     database: 'heroku_cfcebe98f88ba97'
-});
+});*/
 
 
-/*--------Mysql Connection--------
+/*--------Mysql Connection--------*/
 var connection = mysql.createPool({
     connectionLimit: 3,
     host: 'localhost',
     user: 'root123',
     password: 'root123',
     database: 'dbcms'
-});*/
+});
 
 /*------Pages-------*/
 app.get('/',function(req,res){
-	sess=req.session;	
+	sess=req.session;
+	console.log(sess.email);	
 	if(sess.email){
 		res.redirect('/index');
 	}
 	else{res.render('login.html');}
 })
 
-app.get('/index',function(req,res){	
-	res.render('index.html');
+app.get('/index',function(req,res){
+	sess=req.session;
+	console.log(sess.email);
+	if(sess.email)	
+	{
+		res.render('index.html');
+	}
+	else
+	{
+		res.write('<h1>Please login first.</h1>');
+		res.end('<a href='+'/'+'>Login</a>');
+	}
+	
 })
 
 app.get('/SignUp',function(req,res){	
@@ -69,16 +70,16 @@ app.get('/SignUp',function(req,res){
 
 app.post('/restService',function(req,res){
 	sess=req.session;
-	sess.reqUser = req.body.txtEmail;
+	sess.email = req.body.txtEmail;
 	var reqPass = req.body.txtPass;	
-	var txtPass = encrypt(reqPass);
-	var sessionId = encrypt(sess.reqUser);
-	var _selQuery = "select * from tblregister where txtusername='"+sess.reqUser+"'";
+	var txtPass = reqPass;
+	var sessionId = sess.email;
+	var _selQuery = "select * from tblregister where txtusername='"+sess.email+"'";
 	connection.query(_selQuery,function(err, rows, fields){
 		 if(err) { console.log(err.message);}
 		 else { 
 		 		if((rows.length)>0){
-					var _qqPass = "select * from tblregister where txtusername='"+sess.reqUser+"' and txtpassword='"+txtPass+"'";
+					var _qqPass = "select * from tblregister where txtusername='"+sess.email+"' and txtpassword='"+txtPass+"'";
 						connection.query(_qqPass,function(err, rows, fields){
 							 if(err) { console.log(err.message)}
 							 else{
@@ -89,7 +90,7 @@ app.post('/restService',function(req,res){
 											var qqUpdate = "update tblregister set txtsession='"+sessionId+"',txtIP='"+ip.address()+"',datetime=NOW() where id='"+_logId+"'";						
 											connection.query(qqUpdate,function(err,rows,fields){
 												if(err) {console.log(err.message);}
-												else{res.send({response:'success',username: sess.reqUser});	}
+												else{res.send({response:'success',username: sess.email});	}
 											})
 										}										 
 										 }
@@ -103,31 +104,7 @@ app.post('/restService',function(req,res){
 				else{
 					res.send({response:'WrongUserPass'});
 				}
-			  }
-		 /*
-		 	var _qqPass = "select * from tblregister where txtusername='"+sess.reqUser+"' and txtpassword='"+txtPass+"'";
-					connection.query(_selQuery,function(err, rows, fields){
-						 if(err) { console.log(err.message)}
-						 else 
-						 	{
-								res.send({response:'UserPass'}); 
-							}
-					})
-		 else{ 
-				if((rows.length)>0){
-					for(var i in rows){
-						var _logId = rows[i].id;
-						var qqUpdate = "update tblregister set txtsession='"+sessionId+"',txtIP='"+ip.address()+"',datetime=NOW() where id='"+_logId+"'";						
-						connection.query(qqUpdate,function(err,rows,fields){
-							if(err) {console.log(err.message);}
-							else{res.send({response:'success',username: sess.reqUser});	}
-						})
-					}
-				}
-				else{
-					res.end('wrong');
-				}
-			}*/
+			  }		 
 	})
 })
 
@@ -136,7 +113,7 @@ app.post('/regiService',function(req,res){
 	var reqFirstName = req.body.txtFirstName;
 	var reqUsername = req.body.txtEmailid;
 	var reqPassword = req.body.txtPass;		
-	var txtPass = encrypt(reqPassword);		
+	var txtPass = reqPassword;		
 	var _selQuery = "select * from tblregister where txtusername='"+reqUsername+"'";
 	connection.query(_selQuery,function(err, rows, fields){
 		 if (err) { console.log(err.message);}
@@ -206,6 +183,19 @@ app.post('/delService',function(req,res){
 	 })
 })
 
+app.get('/logout',function(req,res){
+	
+	req.session.destroy(function(err){
+		if(err){
+			console.log(err);
+		}
+		else
+		{
+			res.redirect('/');
+		}
+	});
+
+});
 app.get('*',function(req,res){
 	res.send('404 Error page not found');
 })
